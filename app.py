e6d2b8d2af881b0ddab7f870afd874feee5356c8
jni3355 <wlsgml3835@gmil.com>
2021-12-07 13:04:24 +0900

    추가

--- app.py
+++ app.py
@@ -26,9 +26,11 @@ def memberlist():
 def member_view(id):    #mid를 경로로 설정하고 매개변수로 넘겨준다.
     conn = getconn()
     cur = conn.cursor()
-    sql = "SELECT * FROM member WHERE mid='%s'" %(id)
+    sql = "SELECT * FROM member WHERE mid='%s'" % (id)
     cur.execute(sql)
     rs = cur.fetchall()     #해당 1개의 자료를 반환받음
+    #목록을 불러올때는 fetchall()을 사용한다.
+    #목록을 1개씩 받아올때는 fetchone()을 사용한다.
     conn.close()
     return render_template('member_view.html', rs=rs)
 
