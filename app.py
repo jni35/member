b4ead9e9dc112767ebc819c45485977078a78d9d
jni3355 <wlsgml3835@gmil.com>
2021-12-07 12:54:32 +0900

    member추가

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
+
+def getconn():
+    conn = sqlite3.connect('./memberdb.db')
+    return conn
 
 @app.route('/')
 def index():
     return render_template('index.html')
     # return"<h1>Welcome~ 방문을 환영합니다.</h1>"
+
+@app.route('/memberlist/')
+def memberlist():
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "SELECT * FROM member"
+    cur.execute(sql)
+    rs = cur.fetchall() #db에서 검색한 테이터
+    conn.close()
+    return render_template('memberlist.html', rs=rs)
+
+@app.route('/member_view/<string:id>/')
+def member_view(id):    #mid를 경로로 설정하고 매개변수로 넘겨준다.
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "SELECT * FROM member WHERE mid='%s'" %(id)
+    cur.execute(sql)
+    rs = cur.fetchall()     #해당 1개의 자료를 반환받음
+    conn.close()
+    return render_template('member_view.html', rs=rs)
 
 app.run(debug=True)
